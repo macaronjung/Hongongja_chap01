81df53319b492581f6885ac7f90b296c15901fe2
Computer <Computer@DESKTOP-CK03VLE>
2020-01-14 23:31:09 +0900

    initial commit

---
+++ src/sec03/exam01/Hello.java
@@ -0,0 +1,16 @@
+package sec03.exam01;
+
+/*
+ 작성자:박문각
+ 작성일:2019년 8월 14일
+ */
+
+public class Hello {
+//프로그램 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+
+	} //end of main
+
+} //end of class
---
+++ src/sec03/exam02/RunStatementExample.java
@@ -0,0 +1,18 @@
+package sec03.exam02;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		//변수 x를 선언하고 1을 저장
+		int x = 1;
+		//변수 y를 선언하고 2를 저장
+		int y = 2;
+		//변수 result를 선언하고 x+y 값을 저장
+		int result = x + y;
+	
+		//메소드 호출
+		System.out.println(result);
+
+	}
+
+}
